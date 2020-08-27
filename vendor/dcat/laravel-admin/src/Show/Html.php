9075568b46a40740db119068826d4cfee917900a
yels <narwhal_yel@126.com>
2020-08-27 09:22:25 +0800

    vendor

---
+++ vendor/dcat/laravel-admin/src/Show/Html.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dcat\Admin\Show;
+
+use Dcat\Admin\Support\Helper;
+
+class Html extends Field
+{
+    public $html;
+
+    public function __construct($html, string $name = '', string $label = '')
+    {
+        $this->html = $html;
+        parent::__construct($name, $label);
+    }
+
+    public function render()
+    {
+        return Helper::render($this->html, [$this->value()], $this->parent->model());
+    }
+}
